SimpleAOSP-Lollipop/platform_packages_apps_Settings

[2/2] Settings: Ambient Display configurations * BaNkS: checkbox > switches
Change-Id: Ib85c25d50dc924d4f07658dac033e6581184317d

src/com/android/settings/search/SearchIndexableResources.java
@@ -42,6 +42,7 @@ import com.android.settings.notification.OtherSoundSettings;
 import com.android.settings.notification.ZenModeSettings;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.sim.SimSettings;
+import com.android.settings.simpleaosp.AmbientSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.voice.VoiceInputSettings;
 import com.android.settings.wifi.AdvancedWifiSettings;
@@ -259,6 +260,13 @@ public final class SearchIndexableResources {
                         NO_DATA_RES_ID,
                         DeviceInfoSettings.class.getName(),
                         R.drawable.ic_settings_about));
+
+        sResMap.put(AmbientSettings.class.getName(),
+                new SearchIndexableResource(
+                        Ranking.getRankForClassName(AmbientSettings.class.getName()),
+                        NO_DATA_RES_ID,
+                        AmbientSettings.class.getName(),
+                        R.drawable.ic_settings_display));
 
 	sResMap.put(StatusBarSettings.class.getName(),
                 new SearchIndexableResource(

TeamGlade/packages_apps_Settings

Change-Id: I2c05e3c9cfe2969c6ba0b381724e52c295eb8bd9

src/com/android/settings/search/SearchIndexableResources.java
@@ -46,6 +46,7 @@ import com.android.settings.notification.OtherSoundSettings;
 import com.android.settings.notification.ZenModeSettings;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.sim.SimSettings;
+import com.android.settings.screwd.AmbientSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.voice.VoiceInputSettings;
 import com.android.settings.wifi.AdvancedWifiSettings;
@@ -265,7 +266,6 @@ public final class SearchIndexableResources {
                         NO_DATA_RES_ID,
                         DeviceInfoSettings.class.getName(),
                         R.drawable.ic_settings_about));
-
         sResMap.put(ButtonSettings.class.getName(),
                 new SearchIndexableResource(
                         Ranking.getRankForClassName(ButtonSettings.class.getName()),
@@ -293,7 +293,13 @@ public final class SearchIndexableResources {
                                 com.android.settings.cyanogenmod.PrivacySettings.class.getName()),
                         R.xml.privacy_settings_cyanogenmod,
                         com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
-                        R.drawable.ic_settings_privacy));
+                        R.drawable.ic_settings_privacy));			
+		sResMap.put(AmbientSettings.class.getName(),
+                new SearchIndexableResource(
+                        Ranking.getRankForClassName(AmbientSettings.class.getName()),
+                        NO_DATA_RES_ID,
+                        AmbientSettings.class.getName(),
+                        R.drawable.ic_settings_display));				
     }
 
     private SearchIndexableResources() {