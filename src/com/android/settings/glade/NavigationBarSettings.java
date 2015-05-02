toyr99/android_packages_apps_Settings-1

Navigation bar custom height
Conflicts:
	res/values/ras_arrays.xml
	res/values/ras_strings.xml
	res/xml/system_settings.xml

Conflicts:
	res/values/simpleaosp_strings.xml

src/com/android/settings/simpleaosp/NavigationBarSettings.java
@@ -1,0 +1,45 @@
+
+package com.android.settings.simpleaosp;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NavigationBarSettings extends SettingsPreferenceFragment implements
+OnPreferenceChangeListener {
+
+    private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+    private ListPreference mNavigationBarHeight;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.navigation_bar_settings);
+
+        mNavigationBarHeight = (ListPreference) findPreference(KEY_NAVIGATION_BAR_HEIGHT);
+        mNavigationBarHeight.setOnPreferenceChangeListener(this);
+        int statusNavigationBarHeight = Settings.System.getInt(getActivity().getApplicationContext()
+                .getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HEIGHT, 48);
+        mNavigationBarHeight.setValue(String.valueOf(statusNavigationBarHeight));
+        mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntry());
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mNavigationBarHeight) {
+            int statusNavigationBarHeight = Integer.valueOf((String) objValue);
+            int index = mNavigationBarHeight.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_HEIGHT, statusNavigationBarHeight);
+            mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntries()[index]);
+        }
+        return true;
+    }
+}

TeamGlade/packages_apps_Settings

Change-Id: I1eae7ded583f75aadc985e379645fde43e94d0dc

src/com/android/settings/glade/NavigationBarSettings.java
@@ -1,0 +1,45 @@
+
+package com.android.settings.glade;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NavigationBarSettings extends SettingsPreferenceFragment implements
+OnPreferenceChangeListener {
+
+    private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+    private ListPreference mNavigationBarHeight;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.navigation_bar_settings);
+
+        mNavigationBarHeight = (ListPreference) findPreference(KEY_NAVIGATION_BAR_HEIGHT);
+        mNavigationBarHeight.setOnPreferenceChangeListener(this);
+        int statusNavigationBarHeight = Settings.System.getInt(getActivity().getApplicationContext()
+                .getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HEIGHT, 48);
+        mNavigationBarHeight.setValue(String.valueOf(statusNavigationBarHeight));
+        mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntry());
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mNavigationBarHeight) {
+            int statusNavigationBarHeight = Integer.valueOf((String) objValue);
+            int index = mNavigationBarHeight.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_HEIGHT, statusNavigationBarHeight);
+            mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntries()[index]);
+        }
+        return true;
+    }
+}